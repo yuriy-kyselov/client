8384ec37d7830097147edfe146e784cd8b5ee9b3
Yugin <yuriy.kyselov@matreshka.tv>
2026-01-29 17:22:58 +0300

    Update Responses.php

--- src/Resources/Responses.php
+++ src/Resources/Responses.php
@@ -62,7 +62,7 @@ final class Responses implements ResponsesContract
 
         $response = $this->transporter->requestStream($payload);
 
-        Log::debug('Stream Event', $response->getBody())
+        Log::debug('Stream Event', $response->getBody());
 
         return new StreamResponse(CreateStreamedResponse::class, $response);
     }
